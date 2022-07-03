c0e4f8a7b4b591ec19c4394d5d6eff421442cb94
lukasz9103 <lukaszbogacz08@gmail.com>
2022-07-03 21:17:46 +0200

    Webpack basic configuration

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './scss/main.scss';
+
+const customVariable = "My name is Lukas";
+
+console.log(customVariable);
---
+++ webpack.common.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: {
+    app: path.resolve(__dirname, 'src/index.js')
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    clean: true,
+    assetModuleFilename: '[name][ext]'
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css"
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'img/[name][ext]',
+        }
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[name][ext][query]',
+        }
+      },
+    ]
+  }
+}
